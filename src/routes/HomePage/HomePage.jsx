d58c947dc3bddcb7bff11f2dc5a99bb626828abc
fizalfada <justinsp1re212@gmail.com>
2024-06-20 20:59:58 +0700

    search bar design and hero section

---
+++ src/routes/HomePage/HomePage.jsx
@@ -0,0 +1,40 @@
+import SearchBar from "../../components/SearchBar/SearchBar";
+import "./HomePage.scss";
+
+function HomePage() {
+  return (
+    <div className="homePage">
+      <div className="textContainer">
+        <div className="wrapper">
+          <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Cum,
+            aperiam quod. Ullam magnam harum ratione nemo placeat dolorem, fuga
+            consequatur sapiente velit dolore repellendus suscipit ab sequi
+            neque natus voluptate?
+          </p>
+          <SearchBar />
+          <div className="boxes">
+            <div className="box">
+              <h1>16+</h1>
+              <h2>Years of Experience</h2>
+            </div>
+            <div className="box">
+              <h1>200</h1>
+              <h2>Award Gained</h2>
+            </div>
+            <div className="box">
+              <h1>2000+</h1>
+              <h2>Property Ready</h2>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="imgContainer">
+        <img src="/bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
