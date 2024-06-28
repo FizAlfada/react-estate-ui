6981609bb7a876a3af459e6a5d099606c0c1dda7
fizalfada <justinsp1re212@gmail.com>
2024-06-28 22:06:40 +0700

    add react router and layout route

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,44 @@
-import Navbar from "./components/Navbar/Navbar";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import Navbar from "./components/Navbar/Navbar";
 import "./layout.scss";
 import HomePage from "./routes/HomePage/HomePage";
+import ListPage from "./routes/ListPage/ListPage";
+import Layout from "./routes/Layout/Layout";
+import Login from "./routes/Login/Login";
+import SinglePage from "./routes/SinglePage/SinglePage";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+      ],
+    },
+  ]);
+
   return (
-    <div className="layout">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="content">
-        <HomePage />
-      </div>
-    </div>
+    <RouterProvider router={router} />
+    // <div className="layout">
+    //   <div className="navbar">
+    //     <Navbar />
+    //   </div>
+    //   <div className="content">
+    //     <HomePage />
+    //   </div>
+    // </div>
   );
 }
 
